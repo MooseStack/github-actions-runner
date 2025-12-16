05e82b72e6f244f71b87dadf817cf3aedfcef2cf
Mustafa Bell <63203400+MooseStack@users.noreply.github.com>
2025-12-16 01:58:41 -0600

    gh runner, rootless image

---
+++ container_image/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -eE
+
+CREDS_FILE="${PWD}/.credentials"
+
+# Assume registration artifacts have been persisted from a previous start
+# if no PAT or TOKEN is provided, and simply attempt to start.
+if [ -n "${GITHUB_PAT:-}" ] || [ -n "${RUNNER_TOKEN:-}" ] || [ -n "${GITHUB_APP_ID:-}" ]; then
+    source /entrypoint/register.sh
+elif [ -e "${CREDS_FILE}" ]; then
+    echo "No GITHUB_PAT or RUNNER_TOKEN provided. Using existing credentials file ${CREDS_FILE}."
+else
+    echo "No saved credentials found in ${CREDS_FILE}."
+    echo "Fatal: GITHUB_PAT or RUNNER_TOKEN must be set in the environment."
+    exit 1
+fi
+
+if [ -n "${GITHUB_PAT:-}" ]; then
+    trap 'remove; exit 130' INT
+    trap 'remove; exit 143' TERM
+elif [ -n "${GITHUB_APP_ID:-}" ]; then
+    trap 'remove_github_app; exit 130' INT
+    trap 'remove_github_app; exit 143' TERM
+else
+    trap 'exit 130' INT
+    trap 'exit 143' TERM
+fi
+
+set -x
+/opt/gh-actions-runner/run.sh --once &
+svc_pid=$!
+
+wait $svc_pid
---
+++ container_image/register.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+# Based on https://github.com/bbrowning/github-runner/blob/master/entrypoint.sh
+
+set -eE
+
+# Load Github app authentication helper function
+source /entrypoint/get_github_app_token.sh
+
+if [ -z "${GITHUB_OWNER:-}" ]; then
+    echo "Fatal: \$GITHUB_OWNER must be set in the environment"
+    exit 1
+fi
+
+if [ -z "${GITHUB_DOMAIN:-}" ]; then
+    echo "Connecting to public GitHub"
+    GITHUB_DOMAIN="github.com"
+    GITHUB_API_SERVER="api.github.com"
+else
+    echo "Connecting to GitHub server at '$GITHUB_DOMAIN'"
+    GITHUB_API_SERVER="${GITHUB_DOMAIN}/api/v3"
+fi
+
+echo "GitHub API server is '$GITHUB_API_SERVER'"
+
+if [ -z "${GITHUB_REPOSITORY:-}" ] && [ -n "${GITHUB_REPO:-}" ]; then
+    GITHUB_REPOSITORY=$GITHUB_REPO
+fi
+
+# https://docs.github.com/en/free-pro-team@latest/rest/reference/actions#create-a-registration-token-for-an-organization
+
+registration_url="https://${GITHUB_DOMAIN}/${GITHUB_OWNER}${GITHUB_REPOSITORY:+/$GITHUB_REPOSITORY}"
+
+if [ -z "${GITHUB_PAT:-}" ] && [ -z "${GITHUB_APP_ID:-}" ]; then
+    echo "Neither GITHUB_PAT nor the GITHUB_APP variables are set in the environment. Automatic runner removal will be disabled."
+    echo "Visit ${registration_url}/settings/actions/runners to manually force removal of runner."
+fi
+
+if [ -z "${RUNNER_TOKEN:-}" ]; then
+    if [ -z "${GITHUB_REPOSITORY:-}" ]; then
+        echo "Runner is scoped to organization '${GITHUB_OWNER}'"
+        echo "View runner status at https://${GITHUB_DOMAIN}/organizations/${GITHUB_OWNER}/settings/actions"
+
+        token_url="https://${GITHUB_API_SERVER}/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
+    else
+        echo "Runner is scoped to repository '${GITHUB_OWNER}/${GITHUB_REPOSITORY}'"
+        echo "View runner status at https://${GITHUB_DOMAIN}/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/settings/actions"
+
+        token_url="https://${GITHUB_API_SERVER}/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+    fi
+    echo "Obtaining runner token from ${token_url}"
+
+    if [ -n "${GITHUB_APP_ID:-}" ] && [ -n "${GITHUB_APP_INSTALL_ID:-}" ] && [ -n "${GITHUB_APP_PEM:-}" ]; then
+        echo "GITHUB_APP environment variables are set. Using GitHub App authentication."
+        app_token=$(get_github_app_token)
+        payload=$(curl -sSfLX POST -H "Authorization: token ${app_token}" ${token_url})
+    else
+        echo "Using GITHUB_PAT for authentication."
+        payload=$(curl -sSfLX POST -H "Authorization: token ${GITHUB_PAT}" ${token_url})
+    fi
+
+    export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+    echo "Obtained registration token"
+else
+    echo "Using RUNNER_TOKEN from environment"
+fi
+
+
+if [ -n "${RUNNER_TOKEN:-}" ]; then
+    set -x
+    /opt/gh-actions-runner/config.sh \
+        --name ubi9-$(date +%m-%d-%Y.%s) \
+        --token ${RUNNER_TOKEN} \
+        --url ${registration_url} \
+        --work ${RUNNER_WORKDIR} \
+        --labels ${RUNNER_LABEL} \
+        --ephemeral \
+        --unattended \
+        --replace
+    set +x
+fi
+
+remove() {
+    payload=$(curl -sSfLX POST -H "Authorization: token ${GITHUB_PAT}" ${token_url%/registration-token}/remove-token)
+    export REMOVE_TOKEN=$(echo $payload | jq .token --raw-output)
+
+    /opt/gh-actions-runner/config.sh remove --unattended --token "${REMOVE_TOKEN}"
+}
+
+remove_github_app() {
+    app_token=$(get_github_app_token)
+    payload=$(curl -sSfLX POST -H "Authorization: token ${app_token}" ${token_url%/registration-token}/remove-token)
+    export REMOVE_TOKEN=$(echo $payload | jq .token --raw-output)
+
+    /opt/gh-actions-runner/config.sh remove --unattended --token "${REMOVE_TOKEN}"
+}
